12d847ddb0bdd3751c270013eac88d449ac98bf5
LooperLian <58178369+LooperLian@users.noreply.github.com>
2019-12-12 15:11:01 +0800

    Update test.java

--- src/test.java
+++ src/test.java
@@ -1,20 +1,27 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
-public class test {
+class test{
     public static void main(String[] args) {
-        mainIntegration m = new mainIntegration();
-        m.initDisMatrix(m.DistanceMatrix);//connectionmatrix
-        m.initNodeSTResource();
-        m.initSpectrumResource();
-        m.initLightPathResource();
-        m.initServiceList();
-        m.updateServiceList();
-        m.autoStopServivice(10);
-        ArrayList<Integer> path = new ArrayList<>();
-        path.add(0);
-        path.add(1);
-        m.addLightPath(0,1,10,true,path);
+        mainIntegration ma = new mainIntegration();
+        ma.init();
+        ma.genService(1,2);
+        ArrayList<Integer[]> serviceSet_genService = new ArrayList<>();    //业务集合，interger为1*2数组，起始点与终点
+        ArrayList<Integer> demandSet_genService = new ArrayList<>();        //业务带宽需求记录集合
+        ArrayList<Double> durationSet_genService = new ArrayList<>();      //业务持续时间记录结合
+        ArrayList<Double> toleranceSet_genService = new ArrayList<>();     //业务（时延？）容忍度
+        serviceSet_genService=ma.getServiceSet_genService();
+        System.out.println(serviceSet_genService.get(0)[0]);
+        System.out.println(serviceSet_genService.get(0)[1]);
+        durationSet_genService=ma.getDurationSet_genService();
+        System.out.println("durationSet_genService"+durationSet_genService);
+        demandSet_genService=ma.getDemandSet_genService();
+        System.out.println("demandSet_genService"+demandSet_genService);
+        toleranceSet_genService=ma.getToleranceSet_genService();
+        System.out.println("toleranceSet_genService"+toleranceSet_genService);
+        ma.Dijkstra(serviceSet_genService.get(0)[0], serviceSet_genService.get(0)[1], demandSet_genService.get(0));
+        ArrayList<ArrayList<Object>> pathij = ma.getPathij_Dijkstra();
+        System.out.println("路径是："+pathij);
+        ma.addService(serviceSet_genService.get(0)[0],serviceSet_genService.get(0)[1],demandSet_genService.get(0),pathij,1,15);
+        //ma.addConnection(13,12,10,1,13,false,true);
     }
 }
